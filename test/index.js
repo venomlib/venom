da68e86fee03973c2718a73f906a5b8b4f05a794
Edwin <9cb14c1ec0@sendmeemail.xyz>
2025-09-08 10:47:15 -0400

    make knip scan only dependencies

--- test/index.js
+++ test/index.js
@@ -5,20 +5,29 @@ const venom = require('../dist');
 venom.create({
     session: 'sessionname', //name of session
     headless: false,
-    logQR: true
+    logQR: true,
+    options: {
+      browserPathExecutable:
+        'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe'
+    }
   })
   .then((client) => {
     start(client);
   });
 
 async function start(client) {
-
   const f = await client.getHostDevice();
   console.log(await client.getWAVersion());
   console.log(f);
-  // client.onMessage((message) => {
-  //   console.log(message);
-  // });
+  client.onMessage(async (message) => {
+    console.log(message);
+    await client.sendImage(
+      message.from,
+      'C:\\Users\\Edwin\\Pictures\\Screenshots\\Screenshot 2025-08-08 130054.png',
+      'test.png',
+      'test'
+    );
+  });
   // const allMessages = await client.getAllUnreadMessages();
   // console.log(allMessages);
 }
