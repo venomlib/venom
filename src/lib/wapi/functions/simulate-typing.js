aca7e330acb28efda431b50a22f9a8403155f4ad
Edwin <9cb14c1ec0@sendmeemail.xyz>
2025-12-22 13:02:57 -0500

    fix lid migration

--- src/lib/wapi/functions/simulate-typing.js
+++ src/lib/wapi/functions/simulate-typing.js
@@ -1,5 +1,6 @@
 import { fixLidMigration } from '../helper/index.js';
 export async function startTyping(chatId) {
+  console.log(`startTyping: chatId ${chatId}`);
   const chat = window.Store.WidFactory.createWid(fixLidMigration(chatId));
   if (!chat) {
     throw {
