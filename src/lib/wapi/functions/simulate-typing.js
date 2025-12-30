da4fb84e33daa528b4a3eb7ca5100d8c1ffc7e2c
Edwin <9cb14c1ec0@sendmeemail.xyz>
2025-12-30 11:48:39 -0500

    more lid migrations

--- src/lib/wapi/functions/simulate-typing.js
+++ src/lib/wapi/functions/simulate-typing.js
@@ -1,15 +1,20 @@
-import { createWidWrapper } from '../helper/index.js';
 export async function startTyping(chatId) {
   console.log(`startTyping: chatId ${chatId}`);
-  const chat = createWidWrapper(chatId);
-  if (!chat) {
-    throw {
-      error: true,
-      code: 'chat_not_found',
-      message: 'Chat not found',
-      chatId: chatId
-    };
+  try {
+    let chat = window.Store.WidFactory.createWid(chatId);
+    if (!chat) {
+      throw {
+        error: true,
+        code: 'chat_not_found',
+        message: 'Chat not found',
+        chatId: chatId
+      };
+    }
+    await Store.ChatStates.sendChatStateComposing(chat);
+  } catch (e) {
+    window.onLog(`Error in startTyping: ${e}`);
+    window.onLog(`ChatId ${chatId}`);
   }
-  await Store.ChatStates.sendChatStateComposing(chat);
+
   return { status: 200 };
 }
